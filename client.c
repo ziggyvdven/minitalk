61ecea0e0e98507649c081fb94259921bee99408
ziggy <ziggyvandeven@gmail.com>
2023-03-22 19:51:05 -0400

    signal
    ;

--- client.c
+++ client.c
@@ -6,18 +6,72 @@
 /*   By: zvan-de- <zvan-de-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 14:54:51 by zvan-de-          #+#    #+#             */
-/*   Updated: 2023/03/21 20:01:29 by zvan-de-         ###   ########.fr       */
+/*   Updated: 2023/03/22 19:30:21 by zvan-de-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"includes/minitalk.h"
 
+int	ft_dtob(int nbr)
+{
+	int		binary_num;
+	int		bit_position;
+	int		bit;
+
+	bit_position = 1;
+	binary_num = 0;
+	while (nbr > 0)
+	{
+		bit = nbr % 2;
+		binary_num += bit * bit_position;
+		nbr /= 2;
+		bit_position *= 10;
+	}
+	return (binary_num);
+}
+
+void	signal_send(char *str, int pid)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '0')
+		{
+			kill (pid, SIGUSR1);
+			usleep(500000);
+		}
+		if (str[i] == '1')
+		{
+			kill (pid, SIGUSR2);
+			usleep(500000);
+		}
+	i++;
+	}
+}
+
 int main(int argc, char **argv)
 {
+	int		pid;
+	int		binary;
+	int		i;
+	char	*binary_str;
+
+	i = 0;
+	pid = ft_atoi(argv[1]);
 	if (argc == 3)
 	{
-		ft_printf("argument 2 = %s\n", argv[2]);
-	} 
-	else
-		return (0);
+	// function that converts an char to a binary 
+		while (argv[2][i])
+		{
+			binary = ft_dtob(argv[2][i]);
+			binary_str = ft_itoa(binary);
+			ft_printf("\nchar = %s\n", binary_str);
+			signal_send(binary_str, pid);
+			i++;
+			sleep(2);
+		}
+	}
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,18 +6,71 @@
 /*   By: zvan-de- <zvan-de-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 20:18:23 by zvan-de-          #+#    #+#             */
-/*   Updated: 2023/03/21 20:18:25 by zvan-de-         ###   ########.fr       */
+/*   Updated: 2023/03/22 19:48:15 by zvan-de-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"includes/minitalk.h"
 
-int main()
-{
-	pid_t pid;
+int bit;
 
+char	ft_btod(int num)
+{
+	int		decimal_num;
+	int		base;
+	int		rem;
+
+	base = 1;
+	decimal_num = 0;
+	while (num > 0)
+	{
+		rem = num % 10;
+		decimal_num = decimal_num + rem * base;
+		num = num / 10;
+		base = base * 2;
+	}
+	return (decimal_num);
+}
+
+void handle_sigusr1()
+{
+	bit = 0;
+}
+
+void handle_sigusr2()
+{
+	bit = 1;
+}
+
+int	main()
+{
+	struct sigaction	s1 = {0};
+	struct sigaction	s2 = {0};
+	pid_t				pid;
+	char				byte[8];
+	int 				i;
+
+	i =	0;
+	s1.sa_handler = &handle_sigusr1;
+	s2.sa_handler = &handle_sigusr2;
+	sigaction(SIGUSR1, &s1, NULL);
+	sigaction(SIGUSR2, &s2, NULL);
 	pid = getpid();
 	ft_printf("%d\n", pid);
-	while(1)
-		pause();
+	while (1)
+	{
+		pause ();
+		byte[i] = bit;
+		i++;
+		if(i == 7)
+		{
+			ft_printf("bit 7 = NULL\n");
+			byte[7] = '\0';
+			fflush(stdout);
+			ft_printf("%s\n", byte);
+			// ft_printf("byte = %d%d%d%d%d%d%d\n", byte[0],byte[1],byte[2],byte[3],byte[4],byte[5],byte[6]);
+			i = 0;
+		}
+		usleep(200);
+	}
 }
